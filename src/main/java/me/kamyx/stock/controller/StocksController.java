58615daea42944bfa4e770630fe4fccec25d8e50
matbal6914 <mateusz.grzegorz.balicki@gmail.com>
2024-01-15 10:12:08 +0100

    Sell and buy method v.w

--- src/main/java/me/kamyx/stock/controller/StocksController.java
+++ src/main/java/me/kamyx/stock/controller/StocksController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/stocks")
 public class StocksController {
